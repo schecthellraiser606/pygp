e29c23dbba492817ce0fa67a50153dcfc3577662
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2023-06-20 18:52:00 +0900

    test

--- sandwarm.sh
+++ sandwarm.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 
 python3 keygen.py -n $1 -e $1 -p aaa --public-key-file public.pem --private-key-file private.pem
-echo -e "\n"
+echo "\n"
 python3 sign.py -m "test taksec message" --private-key-file private.pem -p aaa
-echo -e "\n"
+echo "\n"
 cat public.pem
