4b9805f439bd7907831d573b7dd3c09280819859
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2023-06-20 18:06:46 +0900

    passphrase

--- keygen.py
+++ keygen.py
@@ -12,9 +12,9 @@ def generate_keypair(gpg, name, email, passphrase):
     key = gpg.gen_key(input_data)
     return key
 
-def save_keys(gpg, public_key_file, private_key_file, key):
+def save_keys(gpg, public_key_file, private_key_file, key, passphrase):
     public_key = gpg.export_keys(key.fingerprint)
-    private_key = gpg.export_keys(key.fingerprint, secret=True)
+    private_key = gpg.export_keys(key.fingerprint, secret=True, passphrase=passphrase)
 
     with open(public_key_file, "w") as f:
         f.write(public_key)
@@ -33,7 +33,7 @@ def main():
 
     gpg = gnupg.GPG()
     key = generate_keypair(gpg, args.name, args.email, args.passphrase)
-    save_keys(gpg, args.public_key_file, args.private_key_file, key)
+    save_keys(gpg, args.public_key_file, args.private_key_file, key, args.passphrase)
 
     print("Key pair generated successfully!")
     print(f"Public key saved to: {args.public_key_file}")
