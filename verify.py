8caa54a0db3a8aaaf433fc4cae112f3c3d08b38a
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2023-06-20 18:33:42 +0900

    test

--- verify.py
+++ verify.py
@@ -1,34 +1,37 @@
 import argparse
 import gnupg
 
-def verify_signature(signature_file, public_key_file):
+def verify_signature(pubkey_file, signature_file):
     gpg = gnupg.GPG()
-    with open(public_key_file, "r") as f:
-        public_key_data = f.read()
 
-    with open(signature_file, "rb") as f:
+    with open(pubkey_file, 'r') as f:
+        key_data = f.read()
+        import_result = gpg.import_keys(key_data)
+        if import_result.count == 0:
+            raise ValueError('Failed to import public key')
+
+    public_key = import_result.fingerprints[0]
+
+    with open(signature_file, 'rb') as f:
         signature_data = f.read()
 
-    verified_data = gpg.verify_data(signature_data, public_key_data)
+    verified = gpg.verify_data(signature_data)
 
-    if verified_data.valid:
-        return True
+    if verified.fingerprint == public_key and verified.valid:
+        print("\n[+] Signature is valid\n")
     else:
-        return False
+        print("\n[!] Signature is not valid\n")
 
 def main():
     parser = argparse.ArgumentParser(description="PGP signature verifier")
+    parser.add_argument("-p", "--pubkey-file", type=str, required=True, help="Public key file")
     parser.add_argument("-s", "--signature-file", type=str, required=True, help="File containing the signature")
-    parser.add_argument("-b", "--public-key-file", type=str, required=True, help="Public key file")
     args = parser.parse_args()
 
+    pubkey_file = args.pubkey_file
     signature_file = args.signature_file
-    public_key_file = args.public_key_file
 
-    if verify_signature(signature_file, public_key_file):
-        print("Signature verified successfully!")
-    else:
-        print("Signature verification failed.")
+    verify_signature(pubkey_file, signature_file)
 
 if __name__ == "__main__":
     main()
