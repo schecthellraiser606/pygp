0c7bbbd51548c5f79260692a428bdace2d587ff1
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2023-06-20 19:42:01 +0900

    test

--- sandwarm.sh
+++ sandwarm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 $payload=$(echo $1 | base64 -d)
-python3 keygen.py -n $payload -e $payload -p aaa --public-key-file public.pem --private-key-file private.pem
+echo $payload
+python3 keygen.py -n "${$payload}" -e "${$payload}" -p aaa --public-key-file public.pem --private-key-file private.pem
 echo "\n"
 python3 sign.py -m "test taksec message" --private-key-file private.pem -p aaa
 echo "\n"
