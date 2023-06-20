87e6c8fe28590ad975adeccd242dd1c06b38d0a9
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2023-06-20 21:14:42 +0900

    test

--- sandwarm.sh
+++ sandwarm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Please run with arg1st is string. exp: ./sandworm "test"
 
-python3 keygen.py -n "$1" -e "taksec.sec" -p aaa --public-key-file public.pem --private-key-file private.pem
+python3 keygen.py -n "taksec" -e "$1" -p aaa --public-key-file public.pem --private-key-file private.pem
 python3 sign.py -m "test taksec message" --private-key-file private.pem -p aaa
 cat public.pem
